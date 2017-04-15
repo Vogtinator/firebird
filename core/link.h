41bded0dcba56d4aabfeedc51fd2b3bd4207cce1
Fabian Vogt <fabian@ritter-vogt.de>
2017-04-15 16:36:04 +0200

    Add missing extern "C" to link.h

--- core/link.h
+++ core/link.h
@@ -3,10 +3,18 @@
 #ifndef _H_LINK
 #define _H_LINK
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void send_file(char *filename);
 
 void ti84_io_link_reset(void);
 uint32_t ti84_io_link_read(uint32_t addr);
 void ti84_io_link_write(uint32_t addr, uint32_t value);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
